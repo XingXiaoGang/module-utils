0acf7c476db4faabe307bad1ab477de67d9cb48c
xingxiaogang <xingxiaogang@corp.apus.com>
2017-05-12 17:49:42 +0800

    add:添加hash工具方法

--- src/main/java/com/fenghuo/utils/hash/HashUtils.java
+++ src/main/java/com/fenghuo/utils/hash/HashUtils.java
@@ -1,7 +1,28 @@
 package com.fenghuo.utils.hash;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 
 /**
  * Created by Administrator on 2016/5/28.
@@ -79,5 +100,148 @@ public class HashUtils {
             des += tmp;
         }
         return des;
+    }
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "SmsUtils";
+
+    public static PublicKey getPublicKey(Context context) {
+        if (DEBUG) {
+            Log.d(TAG, "getPublicKey: start");
+        }
+        try {
+            PackageInfo pkgInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            if (pkgInfo.signatures != null && pkgInfo.signatures.length > 0) {
+                byte[] cert = pkgInfo.signatures[0].toByteArray();
+                if (DEBUG) {
+                    Log.d(TAG, "getPublicKey: signatures:" + cert.length);
+                }
+                InputStream input = new ByteArrayInputStream(cert);
+                CertificateFactory cf = CertificateFactory.getInstance("X509");
+                X509Certificate c = (X509Certificate) cf.generateCertificate(input);
+                return c.getPublicKey();
+            }
+        } catch (Exception e) {
+            if (DEBUG) {
+                Log.e(TAG, "getPublicKey: signatures ERRO :", e);
+            }
+        }
+        if (DEBUG) {
+            Log.e(TAG, "getPublicKey: signatures null(debug)");
+        }
+        return null;
+    }
+
+    public static String parse(Context context, byte[] data) {
+        try {
+            if (DEBUG) {
+                Log.v("BlackDownload", "parse ...");
+            }
+//			KeyStore keyStore = CertificateCoder.getKeyStore("debug.keystore", "android");
+//			PrivateKey privateKey = (PrivateKey) keyStore.getKey("androiddebugkey", "android".toCharArray());
+//			PublicKey publicKey = keyStore.getCertificate("androiddebugkey").getPublicKey();
+
+            PublicKey publicKey = getPublicKey(context);
+            if (DEBUG) {
+                Log.d(TAG, "parse: PublicKey :" + publicKey);
+            }
+            byte[] headerLength = new byte[4];
+            System.arraycopy(data, 0, headerLength, 0, 4);
+            int iKeyLength = byteToInt2(headerLength);
+
+            byte[] keyHeader = new byte[iKeyLength];
+            System.arraycopy(data, 4, keyHeader, 0, iKeyLength);
+
+            Cipher cipherRSADecrypt = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipherRSADecrypt.init(Cipher.DECRYPT_MODE, publicKey);
+            byte[] keyHeaderAfterRSA = cipherRSADecrypt.doFinal(keyHeader);
+
+            byte[] content = new byte[data.length - iKeyLength - 4];
+            System.arraycopy(data, iKeyLength + 4, content, 0, data.length - iKeyLength - 4);
+            byte[] dataAfterDesDecrypt = desDecrypt(content, keyHeaderAfterRSA);
+            String result = new String(dataAfterDesDecrypt);
+            return result;
+        } catch (Exception e) {
+            if (DEBUG) {
+                Log.e(TAG, "", e);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * 将int数值转换为占四个字节的byte数组，本方法适用于(高位在前，低位在后)的顺序。 和bytesToInt2（）配套使用
+     */
+    public static byte[] intToBytes2(int value) {
+        byte[] src = new byte[4];
+        src[0] = (byte) ((value >> 24) & 0xFF);
+        src[1] = (byte) ((value >> 16) & 0xFF);
+        src[2] = (byte) ((value >> 8) & 0xFF);
+        src[3] = (byte) (value & 0xFF);
+        return src;
+    }
+
+    public static int byteToInt2(byte[] b) {
+        int mask = 0xff;
+        int temp = 0;
+        int n = 0;
+        for (int i = 0; i < 4; i++) {
+            n <<= 8;
+            temp = b[i] & mask;
+            n |= temp;
+        }
+        return n;
+    }
+
+    /**
+     * byte数组中取int数值，本方法适用于(低位在后，高位在前)的顺序。和intToBytes2（）配套使用
+     */
+    public static int bytesToInt2(byte[] src, int offset) {
+        int value;
+        value = (int) (((src[offset] & 0xFF) << 24)
+                | ((src[offset + 1] & 0xFF) << 16)
+                | ((src[offset + 2] & 0xFF) << 8) | (src[offset + 3] & 0xFF));
+        return value;
+    }
+
+    private static byte[] desEncrypt(byte[] data, byte[] key) throws Exception {
+        SecretKey secretKey = SecretKeyFactory.getInstance("DES").generateSecret(
+                new DESKeySpec(key));
+        Cipher cipher = Cipher.getInstance("DES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, new SecureRandom());
+        return cipher.doFinal(data);
+    }
+
+    private static byte[] desDecrypt(byte[] data, byte[] key) throws Exception {
+        SecretKey secretKey = SecretKeyFactory.getInstance("DES").generateSecret(
+                new DESKeySpec(key));
+        Cipher cipher = Cipher.getInstance("DES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, new SecureRandom());
+        return cipher.doFinal(data);
+    }
+
+    public static String genRandomKey() {
+        String key = String.valueOf(System.currentTimeMillis());
+        return key.substring(key.length() - 8, key.length());
+    }
+
+    private static void write2File(File path, byte[] content)
+            throws IOException {
+        FileOutputStream out = new FileOutputStream(path);
+        out.write(content);
+        out.close();
+    }
+
+    public static byte[] getFileContent(File path) throws IOException {
+        FileInputStream in = new FileInputStream(path);
+        byte[] b = new byte[1024];
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int len;
+        while ((len = in.read(b)) > 0) {
+            baos.write(b, 0, len);
+        }
+        in.close();
+        return baos.toByteArray();
+
     }
 }
